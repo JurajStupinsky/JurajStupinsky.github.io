9c2aa34feb882e8f4b0f29ff9d5e3b1b977e33e4
JurajStupinsky <73020027+JurajStupinsky@users.noreply.github.com>
2020-10-19 16:11:27 +0200

    20 update

--- 20.js
+++ 20.js
@@ -1,2 +1,6 @@
 $('div.cube_p1').text('TEXT');
-
+let names = ['<strong> Juraj</strong>', ' Roman', ' Jozef', ' Andrej']
+names.forEach(element => {
+    console.log(element)
+    $('ul').append('<li class="ol">'+element+'</li>');    
+});
