bce7968819effe400d0db388a119ddf1f230846f
JurajStupinsky <73020027+JurajStupinsky@users.noreply.github.com>
2020-10-20 15:37:45 +0200

    directions update

--- directions.js
+++ directions.js
@@ -16,3 +16,24 @@ if (word = 'cool'){
 else {
   $(".p1_West").text("Power of DOM")
 }
+$('.b_cc0').click(() => {
+  console.log('Yeah, you clicked me')
+  // it's the same as before
+})
+
+$('.b_cc1').click(() => {
+  $('.b_cc0').text('Text change by 2nd button')
+  console.log('Yeah, text changed')
+  })
+$('.b_cc2').click(() => {
+  $('button').css('background', 'green')
+  console.log('Yeah, color changed')
+  // it's the same as before
+})
+$('.b_def').click(() => {
+    $('.b_cc0').text('Console Log')
+    $('button').removeAttr('style')
+    console.log('Yeah, all back to normal')
+  // it's the same as before
+})
+
